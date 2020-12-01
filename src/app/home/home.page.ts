5cc301d88cc507a43e9fc23c85464868084c30d9
thibault <derbomez.thibault@gmail.com>
2020-12-01 17:51:26 +0100

    ion-select

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,20 +1,37 @@
-import { Component } from '@angular/core';
-import { MealdbApiService } from '../mealdb-api.service';
-import { MEALDB_ListItem } from '../model';
+import { Component } from "@angular/core";
+import { MealdbApiService } from "../mealdb-api.service";
+import { MEALDB_Category, MEALDB_ListItem } from "../model";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  selector: "app-home",
+  templateUrl: "home.page.html",
+  styleUrls: ["home.page.scss"],
 })
 export class HomePage {
   meals: MEALDB_ListItem[] | null = null;
+  categories: MEALDB_Category[] | null = null;
+  category: string = "Vegetarian";
 
   constructor(private mealdb: MealdbApiService) {
-    this.mealdb.findByCategory("Vegetarian")
+    this.mealdb
+      .findByCategory(this.category)
       .subscribe((meals: MEALDB_ListItem[]) => {
         this.meals = meals;
-      })
+      });
+
+    this.mealdb
+      .findAllCategorie()
+      .subscribe((categories: MEALDB_Category[]) => {
+        this.categories = categories;
+      });
   }
 
+  onChange($event) {
+    this.category = $event.target.value;
+    this.mealdb
+      .findByCategory(this.category)
+      .subscribe((meals: MEALDB_ListItem[]) => {
+        this.meals = meals;
+      });
+  }
 }
--- src/app/mealdb-api.service.ts
+++ src/app/mealdb-api.service.ts
@@ -1,41 +1,42 @@
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { MEALDB_ListItem, MEALDB_Meal } from './model';
-import { map } from 'rxjs/operators';
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { MEALDB_Category, MEALDB_ListItem, MEALDB_Meal } from "./model";
+import { map } from "rxjs/operators";
 
 const API = {
   ROOT: "https://www.themealdb.com/api/json/v1/1/",
   get FILTER() {
-    return this.ROOT + "filter.php?c="
+    return this.ROOT + "filter.php?c=";
   },
   get LOOKUP() {
-    return this.ROOT + "lookup.php?i="
-  } 
-}
+    return this.ROOT + "lookup.php?i=";
+  },
+  get CATEGORIE() {
+    return this.ROOT + "list.php?c=list";
+  },
+};
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class MealdbApiService {
+  constructor(private http: HttpClient) {}
 
-  constructor(private http: HttpClient) { }
+  findAllCategorie(): Observable<MEALDB_Category[]> {
+    return this.http.get(API.CATEGORIE).pipe(map((res: any) => res.meals));
+  }
 
   findByCategory(categoryName: string): Observable<MEALDB_ListItem[]> {
     return this.http
       .get(API.FILTER + categoryName)
-      .pipe(
-        map((res: any) => res.meals)
-      )
+      .pipe(map((res: any) => res.meals));
   }
 
   findById(mealId: string): Observable<MEALDB_Meal> {
-    return this.http
-      .get(API.LOOKUP + mealId)
-      .pipe(
-        map((res: any) => res.meals[0])
-        // en entrée: { meals: [objet] } => en sortie: objet
-      )
+    return this.http.get(API.LOOKUP + mealId).pipe(
+      map((res: any) => res.meals[0])
+      // en entrée: { meals: [objet] } => en sortie: objet
+    );
   }
-
 }
